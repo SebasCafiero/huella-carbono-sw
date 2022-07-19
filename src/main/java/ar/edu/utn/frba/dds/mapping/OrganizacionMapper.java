991dd2649626a273ac479ee8d78a8b0b7863bc7c
racin <raulsequeira@frba.utn.edu.ar>
2022-07-18 21:14:54 -0300

    Se completa el alta de Organizacion y se sube un archivo de referencia de un posible json para el alta

--- src/main/java/ar/edu/utn/frba/dds/mapping/OrganizacionMapper.java
+++ src/main/java/ar/edu/utn/frba/dds/mapping/OrganizacionMapper.java
@@ -1,25 +1,26 @@
 package ar.edu.utn.frba.dds.mapping;
 
 import ar.edu.utn.frba.dds.lugares.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class OrganizacionMapper {
     public static void map(JSONObject organizacionDTO, Organizacion organizacion){
-        organizacion.setRazonSocial(organizacionDTO.optString("razonSocial"));
-        organizacion.setTipo(TipoDeOrganizacionEnum.valueOf(organizacionDTO.optString("tipoOrganizacion")));
-
-        JSONObject ubicacionGeograficaDTO = organizacionDTO.getJSONObject("ubicacionGeografica");
-        JSONObject coodenadaDTO = ubicacionGeograficaDTO.getJSONObject("coordenada");
+        JSONObject ubicacionGeograficaDTO = organizacionDTO.optJSONObject("ubicacionGeografica");
+        JSONObject coodenadaDTO = ubicacionGeograficaDTO.optJSONObject("coordenada");
+        JSONArray sectoresDTO = organizacionDTO.optJSONArray("sectores");
+        JSONArray medicionesDTO = organizacionDTO.optJSONArray("mediciones");
+        JSONArray trayectosDTO = organizacionDTO.optJSONArray("trayectos");
 
         UbicacionGeografica ubicacionGeografica = new UbicacionGeografica(
                 ubicacionGeograficaDTO.optString("localidad"),
                 new Coordenada(coodenadaDTO.optFloat("latitud"), coodenadaDTO.optFloat("longitud"))
         );
 
+        organizacion.setRazonSocial(organizacionDTO.optString("razonSocial"));
+        organizacion.setTipo(TipoDeOrganizacionEnum.valueOf(organizacionDTO.optString("tipoOrganizacion")));
         organizacion.setUbicacion(ubicacionGeografica);
-
         organizacion.setClasificacionOrganizacion( new ClasificacionOrganizacion(organizacionDTO.optString("clasificacionOrganizacion")));
-
 
     }
 }
