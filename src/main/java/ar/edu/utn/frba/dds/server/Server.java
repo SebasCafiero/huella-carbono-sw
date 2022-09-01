ae755fc7f4bb59f568d099708ec9835734f9d0b1
Aizuk <nickbrandariz@gmail.com>
2022-09-01 16:36:03 -0300

    archivos para heroku... configuracion del main para que trabaje con heroku

--- src/main/java/ar/edu/utn/frba/dds/server/Server.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Server.java
@@ -3,10 +3,23 @@ package ar.edu.utn.frba.dds.server;
 import spark.Spark;
 import spark.debug.DebugScreen;
 
+import static spark.Spark.port;
+
 public class Server {
     public static void main(String[] args) {
-        Spark.port(9000);
+        //port(9000);
         Router.init();
-        DebugScreen.enableDebugScreen();
-    }
+        //DebugScreen.enableDebugScreen();
+
+        port(getHerokuAssignedPort());
+        }
+
+        static int getHerokuAssignedPort() {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            if (processBuilder.environment().get("PORT") != null) {
+                return Integer.parseInt(processBuilder.environment().get("PORT"));
+            }
+            return 9000; //return default port if heroku-port isn't set (i.e. on localhost)
+        }
+
 }
