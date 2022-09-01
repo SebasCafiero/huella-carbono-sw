f7649fab38eae08ee0037944c3c93ee84519ef4b
JulianRuival <julian.i.ruival@gmail.com>
2022-09-01 17:01:36 -0300

    arreglo modificacion de los controllers

--- src/main/java/ar/edu/utn/frba/dds/controllers/BatchMedicionController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/BatchMedicionController.java
@@ -1,7 +1,10 @@
 package ar.edu.utn.frba.dds.controllers;
 
+import ar.edu.utn.frba.dds.entities.lugares.Organizacion;
 import ar.edu.utn.frba.dds.mapping.BatchMedicionMapper;
 import ar.edu.utn.frba.dds.entities.mediciones.BatchMedicion;
+import ar.edu.utn.frba.dds.mapping.FactorEmisionMapper;
+import ar.edu.utn.frba.dds.mapping.OrganizacionMapper;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
@@ -34,9 +37,11 @@ public class BatchMedicionController {
     }
 
     public Object modificar(Request request, Response response){
-        BatchMedicion batchMedicion = this.repositorio.buscar(Integer.valueOf(request.params("id")));
-        //TODO ver el refactor
-        //this.repositorio.modificar(batchMedicion);
+        BatchMedicion batchMedicionActual = this.repositorio.buscar(Integer.valueOf(request.params("id")));
+        BatchMedicion batchMedicionNueva = new BatchMedicion();
+        JSONObject jsonObject = new JSONObject(request.body());
+        BatchMedicionMapper.toEntity(jsonObject);
+        this.repositorio.modificar(batchMedicionActual,batchMedicionNueva);
         return response;
     }
 
