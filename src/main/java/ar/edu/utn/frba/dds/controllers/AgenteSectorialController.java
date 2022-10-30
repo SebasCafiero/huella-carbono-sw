f28f3b7d61a66dae908189c5b313ab46955a5518
Aizuk <nickbrandariz@gmail.com>
2022-10-29 21:34:42 -0300

    Se agregan el control de acceso (pero comentado, ya que por lo pronto estamos devolviendo strings y objects planos en vez de vistas)

--- src/main/java/ar/edu/utn/frba/dds/controllers/AgenteSectorialController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/AgenteSectorialController.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.entities.personas.AgenteSectorial;
+import ar.edu.utn.frba.dds.login.LoginController;
 import ar.edu.utn.frba.dds.mapping.AgenteSectorialMapper;
 import ar.edu.utn.frba.dds.mihuella.dto.AgenteSectorialJSONDTO;
 import ar.edu.utn.frba.dds.mihuella.parsers.ParserJSON;
@@ -13,20 +14,32 @@ import java.util.List;
 
 public class AgenteSectorialController {
     private Repositorio<AgenteSectorial> repositorio;
+    private LoginController loginController;
 
-    public AgenteSectorialController() {this.repositorio = FactoryRepositorio.get(AgenteSectorial.class);}
+    public AgenteSectorialController() {this.repositorio = FactoryRepositorio.get(AgenteSectorial.class);
+    loginController = new LoginController();
+    }
 
     public String mostrarTodos(Request request, Response response) {
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         List<AgenteSectorial> agentes =this.repositorio.buscarTodos();
         return agentes.toString();
     }
 
     public String obtener(Request request, Response response){
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         AgenteSectorial agenteSectorial = this.repositorio.buscar(Integer.parseInt(request.params("id")));
         return agenteSectorial.toString();
     }
 
     public Object agregar(Request request, Response response){
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         AgenteSectorial agenteSectorial = AgenteSectorialMapper.toEntity(
                 new ParserJSON<>(AgenteSectorialJSONDTO.class).parseElement(request.body()));
         this.repositorio.agregar(agenteSectorial);
@@ -34,6 +47,9 @@ public class AgenteSectorialController {
     }
 
     public Object modificar(Request request, Response response){
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         AgenteSectorial agenteSectorial = AgenteSectorialMapper.toEntity(
                 new ParserJSON<>(AgenteSectorialJSONDTO.class).parseElement(request.body()));
         this.repositorio.modificar(Integer.parseInt(request.params("id")),agenteSectorial);
@@ -41,6 +57,9 @@ public class AgenteSectorialController {
     }
 
     public Object eliminar(Request request, Response response){
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         AgenteSectorial agenteSectorial = this.repositorio.buscar(Integer.parseInt(request.params("id")));
         this.repositorio.eliminar(agenteSectorial);
         return "Agente Sectorial eliminado correctamente";
--- src/main/java/ar/edu/utn/frba/dds/controllers/FactorEmisionController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/FactorEmisionController.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.entities.mediciones.FactorEmision;
+import ar.edu.utn.frba.dds.login.LoginController;
 import ar.edu.utn.frba.dds.mapping.FactorEmisionMapper;
 import ar.edu.utn.frba.dds.mihuella.dto.FactorEmisionJSONDTO;
 import ar.edu.utn.frba.dds.mihuella.parsers.ParserJSON;
@@ -14,18 +15,26 @@ import java.util.List;
 
 public class FactorEmisionController {
     private Repositorio<FactorEmision> repositorio;
+    private LoginController loginController;
 
     public FactorEmisionController(){
         this.repositorio = FactoryRepositorio.get(FactorEmision.class);
+        loginController = new LoginController();
     }
 
     public Object modificar(Request request, Response response){
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         FactorEmision factorEmision = FactorEmisionMapper.toEntity(new ParserJSON<>(FactorEmisionJSONDTO.class).parseElement(request.body()));
         this.repositorio.modificar(Integer.parseInt(request.params("id")), factorEmision);
         return "Factor de emision de id : " + request.params("id") + " modificado correctamente.";
     }
 
     public String mostrarTodos(Request request, Response response) { // solo para probar
+        /*if (loginController.chequearValidezAcceso(request, response, true) != null){
+            return loginController.chequearValidezAcceso(request, response, true);
+        }    Todo esto agregar una vez que tengamos la vista*/
         List<FactorEmision> factores = this.repositorio.buscarTodos();
         return factores.toString();
     }
