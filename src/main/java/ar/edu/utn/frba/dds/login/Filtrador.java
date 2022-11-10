dcdbba8bb4d3e622eec97de6fd3370b976f6000d
Aizuk <nickbrandariz@gmail.com>
2022-11-10 20:13:22 -0300

    Se agregan excepciones para control de logeo y se agregan ideas para el filtrado del mismo

--- src/main/java/ar/edu/utn/frba/dds/login/Filtrador.java
+++ src/main/java/ar/edu/utn/frba/dds/login/Filtrador.java
@@ -19,14 +19,48 @@ public class Filtrador {
         }
 
         if(!userUtils.estaLogueado(request)){
-            response.status(403);
+            response.status(401);
             throw new RuntimeException();
         }
 
-        if( path.equals("/miembro") ){
-            System.out.println("Entre al path: "+ path);
+        String rol = userRepository.getRol(request.session().id());
+
+        if(path.equals("/miembro") ||
+                path.equals("/organizacion") ||
+                path.equals("/medicion") ||
+                path.equals("/batchMedicion") ||
+                path.equals("/factorEmision") ||
+                path.equals("/agenteSectorial") ||
+                path.equals("/trayectos") ||
+                path.equals("/reportes") ||
+                path.equals("/reinicia")){
+            if (!filtrarPorRol("admin",rol){
+                response.status(403);
+                throw new RuntimeException();
+            }
+            return;
         }
+        if()
+    }
 
+    private static boolean filtrarPorRol(String rolDeAceptacion, String rolProvisto){
+        return rolDeAceptacion.equals(rolProvisto);
+    }
+    public static void filtrarPorRol(Request req, Response res, String rolDeAceptacion) throws ForbiddenException{
+        String rol = userRepository.getRol(req.session().id());
+        if(!rolDeAceptacion.equals(rol)) {
+            res.status(403);
+            throw new ForbiddenException();
+        };
+    }
+    private static boolean filtrarPorId(int idDeAceptacion, int idProvista) throws ForbiddenException{
+        return idDeAceptacion == idProvista;
+    }
+    private static void filtrarPorId(Request req, Response res, int idDeAceptacion) throws UnauthorizedException{
 
+        if (!String.valueOf(idDeAceptacion).equals(req.session().id())){
+            res.status(403);
+            throw new UnauthorizedException();
+        }
     }
 }
---
+++ src/main/java/ar/edu/utn/frba/dds/login/UnauthorizedException.java
@@ -0,0 +1,15 @@
+package ar.edu.utn.frba.dds.login;
+
+public class UnauthorizedException extends Throwable{
+
+    private int codigo;
+
+    public UnauthorizedException() {
+        this.codigo = 401;
+    }
+
+    public String getMessage() {
+        return codigo + " Unauthorized";
+    }
+
+}
